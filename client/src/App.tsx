efa869d0b4dac5a45d8245f170eaa1a853aee85f
Dhruv Chandak <dhruvchandak5@gmail.com>
2024-06-04 23:59:43 +0530

    Added Links

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Sender from "./components/Sender";
 import Reciever from "./components/Reciever";
@@ -10,6 +10,10 @@ function App() {
         <Route path="/sender" element={<Sender />} />
         <Route path="/reciever" element={<Reciever />} />
       </Routes>
+      <div>
+        <Link to="/sender">Sender</Link>
+        <Link to="/reciever">Reciever</Link>
+      </div>
     </BrowserRouter>
   );
 }
